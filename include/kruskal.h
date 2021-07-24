201af264644e132eb5d6aa42fe6b3982510c2641
Pravin Aryal <pravinaryal010@gmail.com>
2021-07-24 16:01:56 +0545

    adding

---
+++ include/kruskal.h
@@ -0,0 +1,24 @@
+#pragma once
+
+
+class Edge{
+
+    private:
+
+    char node_1;  //starting node
+    char node_2;  // ending node
+
+    int weight;   // the weight between the two nodes.....
+ 
+
+
+    public:
+
+    bool compare(Edge e1,Edge e2);
+    
+
+
+
+    
+
+};
