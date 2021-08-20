b374457e2b2fa8777d90a2e6a41c9687044506f6
Pravin Aryal <pravinaryal010@gmail.com>
2021-08-20 23:18:35 +0545

    adding main file

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,15 @@
 #include <iostream>
 #include "kruskal.h"
 
-int main(){
+int main()
+{
+    Graph g(4);
+    g.add_edge(0, 1, 2);
+    g.add_edge(1, 2, 1);
+    g.add_edge(0, 3, 1);
+    g.add_edge(2, 3, 3);
 
-
-
-    
+    g.Kruskal_Algorithm();
+    g.print();
+    return 0;
 }
