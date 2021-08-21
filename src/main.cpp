f4f2dd9b63aa64fe80834771217e21c2641d877b
Pravin Aryal <pravinaryal010@gmail.com>
2021-08-21 11:43:54 +0545

    adding main

--- src/main.cpp
+++ src/main.cpp
@@ -11,5 +11,34 @@ int main()
 
     g.Kruskal_Algorithm();
     g.print();
-    return 0;
+
+    std::cout << std::endl;
+
+    Graph g1(6);
+    g1.add_edge(1, 4, 5);
+    g1.add_edge(1, 3, 2);
+    g1.add_edge(2, 4, 3);
+    g1.add_edge(5, 4, 9);
+    g1.add_edge(8, 3, 2);
+    g1.add_edge(2, 5, 7);
+    g1.add_edge(2, 8, 3);
+    g1.add_edge(1, 8, 1);
+
+    g1.Kruskal_Algorithm();
+    g1.print();
+
+    std::cout << std::endl;
+
+    Graph g2(6);
+    g2.add_edge(1, 2, 4);
+    g2.add_edge(0, 2, 5);
+    g2.add_edge(1, 0, 4);
+    g2.add_edge(2, 5, 3);
+    g2.add_edge(3, 2, 3);
+    g2.add_edge(3, 4, 3);
+    g2.add_edge(4, 2, 4);
+    g2.add_edge(5, 3, 2);
+    g2.add_edge(5, 4, 3);
+    g2.Kruskal_Algorithm();
+    g2.print();
 }
